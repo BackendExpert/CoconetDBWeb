a6ecc34e2842ae6888f695c661846e3612462cbf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-16 11:36:12 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='bg-white shadow-md border-b border-gray-200 py-4'>
+        <div className="mx-32">
+            <div className="flex justify-between">
+                <div className="flex">
+                    <img src="https://avatars.githubusercontent.com/u/189954645?s=48&v=4" alt="" />
+                    <h1 className="text-xl pt-3 text-yellow-600 uppercase font-semibold">CoconutDB Web</h1>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
