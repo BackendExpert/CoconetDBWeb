be61f69b7518eaed3446429978bb2ac610105e75
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-16 11:31:42 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CWeb from "./Pages/MyServer/CWeb";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<CWeb /> } />
       </Routes>
